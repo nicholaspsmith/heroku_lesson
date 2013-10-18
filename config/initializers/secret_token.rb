b78b5fcae37ca345b6227497fef48eec7f1ce74a
Nicholas P Smith <npsmith1990@gmail.com>
2013-10-18 10:07:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HerokuLesson::Application.config.secret_key_base = '28c0cd004adce2bd9449c1eafd77454ba162ed30e2775905fbdc56922c5bb658dc326e734723fc89a569ef0c34fa237047502051382e61a604d35dca08196052'
