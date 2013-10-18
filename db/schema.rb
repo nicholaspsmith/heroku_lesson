b2f67b129f75b36713b520b1585abc22d9e3c4d1
Nicholas P Smith <npsmith1990@gmail.com>
2013-10-18 10:25:36 -0500

    Switch sqlite to Postgresql in Gemfile and database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131018150907) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "monkeys", force: true do |t|
     t.string   "name"
     t.string   "age"
